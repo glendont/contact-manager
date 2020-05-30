51b152e25f34dc041400eabfe4eb9b2944946305
Glendon Thaiw <glendon888@gmail.com>
2020-05-30 22:17:29 +0800

    Edit Contact

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,47 @@
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from "../types";
+
+// A reducer is just a function that takes in a state and an action that changes the state
+export default (state, action) => {
+  switch (action.type) {
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) =>
+          contact.id === action.payload.id ? action.payload : contact
+        ),
+      };
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id.toString() !== action.payload.toString()
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
